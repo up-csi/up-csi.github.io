3427ffecbb7cf4f7373dadc338ad4e679fd8e0cf
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-19 16:11:40 +0800

    perf: remove unused CSS in final bundle

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+    experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.html'],
     theme: { fontFamily: { sans: ['Panamera', 'ui-sans-serif', 'system-ui'] } },
 } satisfies Config;
