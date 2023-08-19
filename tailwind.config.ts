765ba0cf0bf468fd86f198ba53d9756744c2c9a9
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-19 15:49:20 +0800

    refactor: migrate to Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+    content: ['./src/**/*.html'],
+    theme: { fontFamily: { sans: ['Panamera', 'ui-sans-serif', 'system-ui'] } },
+} satisfies Config;
