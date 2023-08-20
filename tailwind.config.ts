0f952f829d3d848eda2820ea33ef3f4c24c87dba
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-21 00:29:19 +0800

    feat: use Tailwind typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import typo from '@tailwindcss/typography';
 
 export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.html'],
-    theme: { fontFamily: { sans: ['Panamera', 'ui-sans-serif', 'system-ui'] } },
-    plugins: [forms],
+    theme: { fontFamily: { heading: ['Panamera', ...defaultTheme.fontFamily.sans] } },
+    plugins: [forms, typo],
 } satisfies Config;
