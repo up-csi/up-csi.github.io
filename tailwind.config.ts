90ef5d872f1a339017d0da31158c3960441c9bdd
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-20 17:54:57 +0800

    feat: install `@tailwindcss/forms`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
 
 export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.html'],
     theme: { fontFamily: { sans: ['Panamera', 'ui-sans-serif', 'system-ui'] } },
+    plugins: [forms],
 } satisfies Config;
