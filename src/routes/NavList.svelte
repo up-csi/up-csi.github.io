3ffdad2b57d6b53c1f675df7722880c6e231b7d1
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-10-31 05:01:37 +0800

    fix: explicitly mention `underline` to include into bundle

--- src/routes/NavList.svelte
+++ src/routes/NavList.svelte
@@ -3,6 +3,7 @@
     $: ({ pathname } = $page.url);
 </script>
 
+<!-- TAILWIND: underline (to be included) -->
 <ul class="flex flex-row justify-end gap-4 underline-offset-4">
     <li><a class="py-4" class:underline={pathname === '/people/'} href="/people/">People</a></li>
     <li><a class="py-4" class:underline={pathname === '/events/'} href="/events/">Events</a></li>
