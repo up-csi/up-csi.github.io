a1ab24db8b338ffafac7d8ad71df536888e5f5d7
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-10-31 04:19:36 +0800

    fix: use less aggressive CSS pruner

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,11 @@
 import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vite';
 import pruneVar from 'postcss-prune-var';
-import purgeCss from '@fullhuman/postcss-purgecss';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwind from 'tailwindcss';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    css: {
-        postcss: {
-            plugins: [
-                tailwind,
-                autoprefixer,
-                pruneVar,
-                purgeCss({
-                    content: ['src/**/*.{css,html,js,svelte,ts}'],
-                    defaultExtractor(content) {
-                        // Once Tailwind is able to prune global variables more effectively,
-                        // we must use third-party PostCSS plugins for the meantime.
-                        // https://github.com/FullHuman/purgecss/issues/796
-                        return content.match(/[\w-/:[\]]+(?<!:)/g) || [];
-                    },
-                }),
-            ],
-        },
-    },
+    plugins: [sveltekit(), purgeCss()],
+    css: { postcss: { plugins: [tailwind, autoprefixer, pruneVar] } },
 });
