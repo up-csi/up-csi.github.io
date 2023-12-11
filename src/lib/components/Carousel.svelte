c10943ea0bc27a05ce512e7e8e864302158430ef
Denise Dee <dbdee@up.edu.ph>
2023-12-11 23:11:15 +0800

    feat(src/lib/components/Carousel.svelte): add padding on top of cards

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section class="prose max-w-full">
-    <p class="text-center mb-0">We've worked with</p>
+    <p class="text-center mb-8">We've worked with</p>
     <div class="flex flex-wrap justify-center gap-5">
         <CarouselCard />
         <CarouselCard />
