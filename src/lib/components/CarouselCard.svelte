94cf0aa523ba08829ad5cca243eb950a185e84b5
Denise Dee <dbdee@up.edu.ph>
2023-12-11 23:08:47 +0800

    feat(src/lib/components/CarouselCard.svelte): add background color and radius

--- src/lib/components/CarouselCard.svelte
+++ src/lib/components/CarouselCard.svelte
@@ -1,4 +1,8 @@
 <!-- Carousel Card with filler content -->
-<div class="font-dm flex-initial w-56">
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos maxime eligendi eaque non, aperiam nihil iste, odio autem eum fugiat veritatis ipsum inventore atque nisi est, reprehenderit consequatur ex?</p>
+<div class="font-dm flex-initial w-56 bg-blue p-5 rounded-full text-center">
+    <!-- Sample Text -->
+    <p class="m-0">Lorem ipsum dolor sit amet consectetur</p>
+
+    <!-- Sample Logo -->
+    <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" alt="" class="m-auto w-40"> -->
 </div>
