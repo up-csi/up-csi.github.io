dddf4a72c392fd2acab75d2385c3edd837d3b158
Denise Dee <dbdee@up.edu.ph>
2023-12-11 17:51:24 +0800

    feat(src/lib/components/CarouselCard.svelte): create card for carousel

---
+++ src/lib/components/CarouselCard.svelte
@@ -0,0 +1,4 @@
+<!-- Carousel Card with filler content -->
+<div class="font-dm flex-initial w-56">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique dignissimos maxime eligendi eaque non, aperiam nihil iste, odio autem eum fugiat veritatis ipsum inventore atque nisi est, reprehenderit consequatur ex?</p>
+</div>
