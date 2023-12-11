9fe873316a642fe8d2ac1315bacabfb9d522a758
Denise Dee <dbdee@up.edu.ph>
2023-12-11 18:02:58 +0800

    feat(src/lib/components/Carousel.svelte): create main carousel

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,14 @@
+<script>
+    import CarouselCard from "./CarouselCard.svelte";
+</script>
+
+<section class="prose max-w-full">
+    <p class="text-center mb-0">We've worked with</p>
+    <div class="flex flex-wrap justify-center gap-5">
+        <CarouselCard />
+        <CarouselCard />
+        <CarouselCard />
+        <CarouselCard />
+        <CarouselCard />
+    </div>
+</section>
