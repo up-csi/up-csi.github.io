5f2d21a2bd2309850bbf7523fa2f50d66aea8879
rue-22 <cssalces@up.edu.ph>
2023-12-12 23:49:16 +0800

    fix(src/lib/components/Buttons.svelte): fix spelling error

--- src/lib/components/Buttons.svelte
+++ src/lib/components/Buttons.svelte
@@ -1,6 +1,6 @@
 <script>
     import BlackButton from '$lib/components/BlackButton.svelte';
-    import BlueButton from '$lib/components/BlueBotton.svelte';
+    import BlueButton from '$lib/components/BlueButton.svelte';
 </script>
 
 <BlackButton />
