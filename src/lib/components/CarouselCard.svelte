9192121a9fa46e1aba05a804aeffce13129680f1
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-12-13 21:35:13 +0800

    chore: remove extra comments

--- src/lib/components/CarouselCard.svelte
+++ src/lib/components/CarouselCard.svelte
@@ -2,7 +2,5 @@
 <div class="w-56 flex-initial rounded-full bg-blue p-5 text-center font-dm">
     <!-- Sample Text -->
     <p class="m-0">Lorem ipsum dolor sit amet consectetur</p>
-
     <!-- Sample Logo -->
-    <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" alt="" class="m-auto w-40"> -->
 </div>
