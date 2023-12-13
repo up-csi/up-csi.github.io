e074e2ba69cd00dc390eab40ab7da9183b55ca76
mkpautin <mkppautin@gmail.com>
2023-12-14 04:35:13 +0800

    chore: run formatters, linters, and builders

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,14 +1,16 @@
 <script>
-    import BlackButton from "./BlackButton.svelte";
-    import lino from "$lib/lino-hero.svg";
+    import BlackButton from './BlackButton.svelte';
+    import lino from '$lib/lino-hero.svg';
 </script>
 
-<section class="flex flex-col flex-wrap items-center sm:flex-row my-20">
-    <div class="sm:w-64 w-48 m-4">
+<section class="my-20 flex flex-col flex-wrap items-center sm:flex-row">
+    <div class="m-4 w-48 sm:w-64">
         <img alt="Cool Lino" src={lino} />
     </div>
-    <div class="sm:text-left text-center sm:w-max w-3/5 px-5">
-        <h1 class="font-dm text-csi-black sm:text-5xl text-3xl mb-6 font-light">Let's innovate together!</h1>
+    <div class="w-3/5 px-5 text-center sm:w-max sm:text-left">
+        <h1 class="mb-6 font-dm text-3xl font-light text-csi-black sm:text-5xl">
+            Let's innovate together!
+        </h1>
         <button><BlackButton /></button>
     </div>
 </section>
