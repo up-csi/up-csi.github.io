6ae12d55287d4fc9ab971c464bcff31dbdd1370c
Marcus Pascual <marcusjoaquim05@gmail.com>
2023-12-13 23:12:02 +0800

    feat: add navbar component for desktop layout

---
+++ src/lib/components/NavbarComponent.svelte
@@ -0,0 +1,28 @@
+<nav>
+    <ul class="flex flex-row gap-4 items-center bg-warm-white justify-between pl-8 pr-1 py-1 rounded-xl">
+        <li>
+            <img src="src/lib/icons/upcsi.svg" alt="UP CSI Logo" class="object-contain" width="18"/>
+        </li>
+        <li>
+            <a href="/home/">Home</a>
+        </li>
+        <li>
+            <a href="/about/">About</a>
+        </li>
+        <li>
+            <a href="/team/">Team</a>
+        </li>
+        <li>
+            <a href="/events/">Events</a>
+        </li>
+        <li>
+            <a href="/projects/">Projects</a>
+        </li>
+        <li>
+            <a class="" href="/newsletter/">Newsletter</a>
+        </li>
+        <li>
+            <button class="bg-csi-blue py-2 px-5 rounded-xl">Get Started</button>
+        </li>
+    </ul>
+</nav>
