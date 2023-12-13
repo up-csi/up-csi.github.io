7cf40307a82a8bea97111f5a17cf8f24da308613
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-12-13 23:34:40 +0800

    chore: remove extra newline

--- src/lib/components/CartoonCard.svelte
+++ src/lib/components/CartoonCard.svelte
@@ -1,7 +1,6 @@
 <div class="w-96 rounded-b-[50px] rounded-t-[180px] bg-csi-blue px-2 pt-20">
     <img src="src/lib/cartoon-card-placeholder.svg" alt="Corporate Cartoon" />
 </div>
-
 <div class="w-96">
     <h1 class="font-dm text-3xl">Develop well-rounded DCS students</h1>
     <p>with fundamental classroom learning through practical experience</p>
