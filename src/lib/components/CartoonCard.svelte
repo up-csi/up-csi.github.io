d37fb3d47d9a7d40df2f7f9b07f947a1b3150fea
privatebench <moodychair24@gmail.com>
2023-12-13 16:49:22 +0800

    feat(src/lib/components/CartoonCard.svelte): add cartoon card component

---
+++ src/lib/components/CartoonCard.svelte
@@ -0,0 +1,13 @@
+<div class="bg-csi-blue rounded-b-[50px] rounded-t-[180px] w-96 px-2 pt-20">
+    <img src="src/lib/cartoon-card-placeholder.svg" alt="Corporate Cartoon">
+</div>
+
+<div class="w-96">
+    <h1 class="font-dm text-3xl">Develop well-rounded DCS students</h1>
+    <p>with fundamental classroom learning through practical experience</p>
+
+    <!-- Attribution for Placeholder Image -->
+    <div class="mt-10 underline text-blue">
+        <a href="https://storyset.com/web">Web illustrations by Storyset</a>
+    </div>
+</div>
