1b922381f29168d5a6c8fb342485836de48d7650
Marcus Pascual <marcusjoaquim05@gmail.com>
2023-12-13 23:20:34 +0800

    chore: run formatters, linters, and builders

--- src/lib/components/NavbarComponent.svelte
+++ src/lib/components/NavbarComponent.svelte
@@ -1,7 +1,14 @@
 <nav>
-    <ul class="flex flex-row gap-4 items-center bg-warm-white justify-between pl-8 pr-1 py-1 rounded-xl">
+    <ul
+        class="flex flex-row items-center justify-between gap-4 rounded-xl bg-warm-white py-1 pl-8 pr-1"
+    >
         <li>
-            <img src="src/lib/icons/upcsi.svg" alt="UP CSI Logo" class="object-contain" width="18"/>
+            <img
+                src="src/lib/icons/upcsi.svg"
+                alt="UP CSI Logo"
+                class="object-contain"
+                width="18"
+            />
         </li>
         <li>
             <a href="/home/">Home</a>
@@ -22,7 +29,7 @@
             <a class="" href="/newsletter/">Newsletter</a>
         </li>
         <li>
-            <button class="bg-csi-blue py-2 px-5 rounded-xl">Get Started</button>
+            <button class="rounded-xl bg-csi-blue px-5 py-2">Get Started</button>
         </li>
     </ul>
 </nav>
