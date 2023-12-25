2924bc0b73600cfef5089c9fac0ef5bdc8a15747
Ehren Castillo <kindaexists@gmail.com>
2023-12-26 05:58:48 +0800

    chore: remove component margins

--- src/lib/components/ContentContainer.svelte
+++ src/lib/components/ContentContainer.svelte
@@ -1,5 +1,5 @@
 <div
-    class="my-6 flex flex-col items-center justify-between gap-12 rounded-3xl bg-warm-white px-8 py-8 md:px-32 md:py-16 lg:flex-row"
+    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-warm-white px-8 py-8 md:px-32 md:py-16 lg:flex-row"
 >
     <div class="flex flex-1 flex-col gap-4">
         <slot name="text" />
