b4aaa3cef8a3ceea50c5050063f1c8280d215e61
Ehren Castillo <ehrencastillo@gmail.com>
2024-03-14 02:30:59 +0800

    feat(Footer.svelte): add styling for mobile screens

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,16 +1,25 @@
-<footer class="flex h-auto w-auto justify-between px-8 font-inter">
-    <div class="w-48 text-left">
-        <p class="mb-4 max-w-fit text-lg">
+<footer class="flex flex-col md:flex-row h-auto w-auto justify-between gap-4 p-20 font-inter text-center text-xl">
+    <div class="flex flex-col items-center md:items-start justify-start gap-9 md:text-left">
+        <p class="text-2xl max-w-64">
             UP CSI is a service-oriented organization for student learning
         </p>
-        <a class="underline" href="mailto:lino@up-csi.org">lino@up-csi.org</a>
-        <p>+63999999999</p>
+        <div>
+            <a class="underline" href="mailto:lino@up-csi.org">lino@up-csi.org</a>
+            <p>+63999999999</p>
+        </div>
     </div>
-    <div class="flex flex-col justify-center text-right underline">
-        <a href="https://www.facebook.com/upcsi">Facebook</a>
-        <a href="https://twitter.com/officialupcsi">Twitter</a>
-        <a href="https://www.linkedin.com/company/up-center-for-student-innovations/">LinkedIn</a>
-        <a href="https://github.com/up-csi">GitHub</a>
-        <a class="mt-4 text-sm" href="https://storyset.com/web">Web illustrations by Storyset</a>
+    <div class="flex flex-col items-center md:items-end justify-start gap-9 md:text-right">
+        <div class="flex flex-col underline">
+            <p><a href="https://www.facebook.com/upcsi">Facebook</a></p>
+            <p><a href="https://twitter.com/officialupcsi">Twitter</a></p>
+            <p><a href="https://www.linkedin.com/company/up-center-for-student-innovations/">LinkedIn</a></p>
+            <p><a href="https://github.com/up-csi">GitHub</a></p>
+        </div>
+        <div>
+            <p>Site built with SvelteKit</p>
+
+            <!-- To remove after placeholder images are replaced -->
+            <a href="https://storyset.com/web" class="text-sm underline">Web illustrations by Storyset</a>
+        </div>
     </div>
 </footer>
