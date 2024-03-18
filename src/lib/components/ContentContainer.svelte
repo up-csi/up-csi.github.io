f8ab34ffe1a06ce0ebde257fcd31ad27e754a702
Eriene Galinato <erienegalinato@gmail.com>
2024-03-18 15:00:03 +0800

    feat(ContentContainer.svelte): add parent div to cartoon cards

--- src/lib/components/ContentContainer.svelte
+++ src/lib/components/ContentContainer.svelte
@@ -1,8 +1,10 @@
 <div
-    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-warm-white px-8 py-8 md:px-32 md:py-16 lg:flex-row"
+    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-warm-white px-8 py-8 md:px-32 md:py-16 xl:flex-row"
 >
     <div class="flex flex-1 flex-col gap-4">
         <slot name="text" />
     </div>
-    <slot name="cards" />
+    <div class="w-full flex flex-row items-center justify-center gap-12">
+        <slot name="cards" />
+    </div>
 </div>
