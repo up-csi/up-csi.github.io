ba123ead2aedc4ba0ded5e8b4f959d8a3b8e2afe
Eriene Galinato <erienegalinato@gmail.com>
2024-03-18 15:07:12 +0800

    feat(CartoonCard.svelte): adjust height, width, spacing of cards

--- src/lib/components/CartoonCard.svelte
+++ src/lib/components/CartoonCard.svelte
@@ -2,8 +2,8 @@
     import src from '$lib/cartoon-card-placeholder.svg';
 </script>
 
-<div class="mb-8 max-h-80 w-1/4">
-    <div class="rounded-b-[50px] rounded-t-[180px] bg-csi-blue px-2 pt-5">
+<div class="mb-8 lg:mb-0 flex flex-col items-center w-3/5 lg:w-2/5 xl:w-3/5">
+    <div class="rounded-b-[50px] rounded-t-[180px] bg-csi-blue px-2 pt-5 w-full">
         <img {src} alt="Corporate Cartoon" />
     </div>
     <div class="leading-6">
