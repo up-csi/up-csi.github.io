6e58048cec3950f07b349302c6bc9c88a3c672f6
Eriene Galinato <erienegalinato@gmail.com>
2024-03-19 19:00:21 +0800

    feat(NavBar.svelte): add mobile menu toggle

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -2,6 +2,8 @@
     import upcsi from '$lib/icons/upcsi.svg';
     import darkmode from '$lib/icons/darkmode.svg'
     import lightmode from '$lib/icons/lightmode.svg'
+
+    let mobileMenu = false;
 </script>
 
 <nav class="flex justify-center items-center">
@@ -34,35 +36,37 @@
             <button class="rounded-lg bg-csi-blue px-5 py-2">Get in Touch</button>
         </li>
         <li class="mx-6 sm:mx-10 my-2 lg:hidden">
-            <button class="rounded-lg bg-black px-5 py-2 text-csi-white">Get Started</button>
+            <button class="rounded-lg bg-black px-5 py-2 text-csi-white" on:click={() => mobileMenu = true}>Get Started</button>
         </li>
     </ul>
 </nav>
 
-<div class="hidden h-svh bg-blue-white flex flex-col">
-    <div class="fixed bottom-0 w-full">
-        <ul class="w-full">
-            <li class="m-4">
-                <a href="/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Home</a>
-                <!-- <img src={darkmode} alt="Toggle Dark Mode" class="h-6 mx-4 dark:hidden object-contain" />
-                <img src={lightmode} alt="Toggle Light Mode" class="h-6 mx-4 hidden dark:block object-contain" /> -->
-            </li>
-            <li class="m-4">
-                <a href="/about/" class="font-dm text-csi-black dark:text-csi-white text-2xl">About</a>
-            </li>
-            <li class="m-4">
-                <a href="/events/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Events</a>
-            </li>
-            <li class="m-4">
-                <a href="/projects/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Projects</a>
-            </li>
-            <li class="m-4">
-                <a href="/news/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Newsletter</a>
-            </li>
-            <li class="m-4">
-                <button class="font-dm text-csi-black dark:text-csi-white text-2xl">Get in Touch</button>
-            </li>
-        </ul>
-        <div class="w-full flex justify-center items-center my-4"><button class="rounded-full bg-[#eff5f7] py-3 px-6">Close</button></div>
-    </div> 
-</div>
+{#if mobileMenu}
+    <div class="h-svh bg-blue-white flex flex-col">
+        <div class="fixed bottom-0 w-full">
+            <ul class="w-full">
+                <li class="m-4">
+                    <a href="/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Home</a>
+                    <!-- <img src={darkmode} alt="Toggle Dark Mode" class="h-6 mx-4 dark:hidden object-contain" />
+                    <img src={lightmode} alt="Toggle Light Mode" class="h-6 mx-4 hidden dark:block object-contain" /> -->
+                </li>
+                <li class="m-4">
+                    <a href="/about/" class="font-dm text-csi-black dark:text-csi-white text-2xl">About</a>
+                </li>
+                <li class="m-4">
+                    <a href="/events/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Events</a>
+                </li>
+                <li class="m-4">
+                    <a href="/projects/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Projects</a>
+                </li>
+                <li class="m-4">
+                    <a href="/news/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Newsletter</a>
+                </li>
+                <li class="m-4">
+                    <button class="font-dm text-csi-black dark:text-csi-white text-2xl">Get in Touch</button>
+                </li>
+            </ul>
+            <div class="w-full flex justify-center items-center my-4"><button class="rounded-full bg-[#eff5f7] py-3 px-6" on:click={() => mobileMenu = false}>Close</button></div>
+        </div> 
+    </div>
+{/if}
