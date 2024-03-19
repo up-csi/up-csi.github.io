30c694872d06a082e9c4ddd87c74aa372d62cdb4
Eriene Galinato <erienegalinato@gmail.com>
2024-03-19 09:43:42 +0800

    feat(ContentContainer.svelte): add one-off background color

--- src/lib/components/ContentContainer.svelte
+++ src/lib/components/ContentContainer.svelte
@@ -1,5 +1,5 @@
 <div
-    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-warm-white px-8 py-8 md:px-32 md:py-16 xl:flex-row"
+    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-[#f6f6f6] dark:bg-[#1d1d1d] px-8 py-8 md:px-32 md:py-16 xl:flex-row"
 >
     <div class="flex flex-1 flex-col gap-4">
         <slot name="text" />
