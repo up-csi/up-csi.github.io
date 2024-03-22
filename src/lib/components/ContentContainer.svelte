8c2306ec06fb1bf9a185f90d60381dc6b71b6835
Eriene Galinato <erienegalinato@gmail.com>
2024-03-22 18:05:30 +0800

    feat(ContentContainer): add background colors to tailwind config

--- src/lib/components/ContentContainer.svelte
+++ src/lib/components/ContentContainer.svelte
@@ -1,5 +1,5 @@
 <div
-    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-[#f6f6f6] px-8 py-8 md:px-32 md:py-16 xl:flex-row dark:bg-[#1d1d1d]"
+    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-csi-white-50 px-8 py-8 md:px-32 md:py-16 xl:flex-row dark:bg-csi-black-50"
 >
     <div class="flex flex-1 flex-col gap-4">
         <slot name="text" />
