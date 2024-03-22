333ba5c7c575f82193825aaba4e0c58deede7827
Eriene Galinato <erienegalinato@gmail.com>
2024-03-22 08:56:29 +0800

    chore: run formatters, linters, and builders

--- src/lib/components/CartoonCard.svelte
+++ src/lib/components/CartoonCard.svelte
@@ -2,8 +2,8 @@
     import src from '$lib/cartoon-card-placeholder.svg';
 </script>
 
-<div class="mb-8 lg:mb-0 flex flex-col items-center w-3/5 lg:w-2/5 xl:w-3/5">
-    <div class="rounded-b-[50px] rounded-t-[180px] bg-csi-blue px-2 pt-5 w-full">
+<div class="mb-8 flex w-3/5 flex-col items-center lg:mb-0 lg:w-2/5 xl:w-3/5">
+    <div class="w-full rounded-b-[50px] rounded-t-[180px] bg-csi-blue px-2 pt-5">
         <img {src} alt="Corporate Cartoon" />
     </div>
     <div class="leading-6">
--- src/lib/components/ContentContainer.svelte
+++ src/lib/components/ContentContainer.svelte
@@ -1,10 +1,12 @@
 <div
-    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-[#f6f6f6] dark:bg-[#1d1d1d] px-8 py-8 md:px-32 md:py-16 xl:flex-row"
+    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-[#f6f6f6] px-8 py-8 md:px-32 md:py-16 xl:flex-row dark:bg-[#1d1d1d]"
 >
     <div class="flex flex-1 flex-col gap-4">
         <slot name="text" />
     </div>
-    <div class="w-full flex flex-col items-center justify-center lg:gap-12 xl:gap-4 lg:flex-row xl:w-1/2 xl:h-full xl:items-start xl:justify-between">
+    <div
+        class="flex w-full flex-col items-center justify-center lg:flex-row lg:gap-12 xl:h-full xl:w-1/2 xl:items-start xl:justify-between xl:gap-4"
+    >
         <slot name="cards" />
     </div>
 </div>
