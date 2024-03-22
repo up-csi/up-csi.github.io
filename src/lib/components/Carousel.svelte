0280bce3c60e001f147076e5e66b5defe138d01e
Eriene Galinato <erienegalinato@gmail.com>
2024-03-22 08:00:07 +0800

    feat(Carousel): make keyed each block

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,11 +1,24 @@
 <script>
     import CarouselCard from './CarouselCard.svelte';
+
+    let partners = [
+        {id: 0, name: "Lorem"},
+        {id: 1, name: "ipsum"},
+        {id: 2, name: "dolor"},
+        {id: 3, name: "sit"},
+        {id: 4, name: "amet"},
+        {id: 5, name: "consectetur"},
+        {id: 6, name: "Lorem"},
+        {id: 7, name: "ipsum"},
+        {id: 8, name: "dolor"},
+        {id: 9, name: "sit"},
+        {id: 10, name: "amet"},
+        {id: 11, name: "consectetur"},
+    ];
 </script>
 
 <div class="flex flex-wrap justify-center gap-5">
-    <CarouselCard />
-    <CarouselCard />
-    <CarouselCard />
-    <CarouselCard />
-    <CarouselCard />
+    {#each partners as partner (partner.id)}
+        <CarouselCard name={partner.name}/>
+    {/each}
 </div>
--- src/lib/components/CarouselCard.svelte
+++ src/lib/components/CarouselCard.svelte
@@ -1,6 +1,12 @@
+<script>
+    /** @type {String} */
+    export let name;
+</script>
+
+
 <!-- Carousel Card with filler content -->
 <div class="w-56 flex-initial rounded-full bg-blue p-5 text-center font-inter">
     <!-- Sample Text -->
-    <p class="m-0">Lorem ipsum dolor sit amet consectetur</p>
+    <p class="m-0">{name}</p>
     <!-- Sample Logo -->
 </div>
