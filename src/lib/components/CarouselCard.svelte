0ffc09ae74455cdd07926c353e30c6e9ded94428
Eriene Galinato <erienegalinato@gmail.com>
2024-03-22 17:46:06 +0800

    feat(CarouselCard.svelte): change p to slot

--- src/lib/components/CarouselCard.svelte
+++ src/lib/components/CarouselCard.svelte
@@ -6,6 +6,6 @@
 <!-- Carousel Card with filler content -->
 <div class="w-56 flex-initial rounded-full bg-blue p-5 text-center font-inter snap-center">
     <!-- Sample Text -->
-    <p class="m-0">{name}</p>
+    <slot class="m-0">{name}</slot>
     <!-- Sample Logo -->
 </div>
