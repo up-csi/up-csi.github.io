b06f12894911f040f80d9dde158bc89566ea710e
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-03-23 00:18:08 +0800

    chore: run formatters, linters, and builders

--- src/routes/about/SectionHeaderTextContainer.svelte
+++ src/routes/about/SectionHeaderTextContainer.svelte
@@ -1,31 +1,31 @@
 <script>
-    import src from "$lib/lino-sablay.svg";
-    import upcsi from "$lib/icons/upcsi.svg";
+    import src from '$lib/lino-sablay.svg';
+    import upcsi from '$lib/icons/upcsi.svg';
     /* TODO: implement the anchors for the buttons so that the page scrolls/jumps to those points when the buttons are clicked */
 </script>
 
-<div class="bg-white p-3 flex flex-col w-full md:flex-row items-center md:items-start gap-5">
+<div class="bg-white flex w-full flex-col items-center gap-5 p-3 md:flex-row md:items-start">
     <!--PLACEHOLDER IMAGES-->
     <div class="w-full md:w-7/12">
-        <img src={src} alt="UP CSI Logo Placeholder" class="md:h-[500px] bg-warm-white rounded-3xl"/>
+        <img {src} alt="UP CSI Logo Placeholder" class="rounded-3xl bg-warm-white md:h-[500px]" />
     </div>
-    <div class="flex flex-col md:w-5/12 font-dm gap-5">
+    <div class="flex flex-col gap-5 font-dm md:w-5/12">
         <div class="flex flex-col items-center md:items-start md:pb-6">
-            <img src={upcsi} alt="Circle placeholder" class="h-20 w-20 p-2 rounded-full bg-black"/>
+            <img src={upcsi} alt="Circle placeholder" class="h-20 w-20 rounded-full bg-black p-2" />
         </div>
-        <h1 class="text-sm">
-            Meet UP Center for Student Innovations
-        </h1>
+        <h1 class="text-sm">Meet UP Center for Student Innovations</h1>
         <p class="text-3xl">
             To create a center where students can innovate with the latest technology.
         </p>
-        <div class='text-center md:text-left'>
+        <div class="text-center md:text-left">
             <button
-            class="rounded-full bg-black px-4 py-2 text-csi-white dark:bg-csi-blue dark:text-csi-white"
-            >Mission and Vision</button>
+                class="rounded-full bg-black px-4 py-2 text-csi-white dark:bg-csi-blue dark:text-csi-white"
+                >Mission and Vision</button
+            >
             <button
-            class="rounded-full bg-black px-4 py-2 text-csi-white dark:bg-csi-blue dark:text-csi-white"
-            >See Team</button>
+                class="rounded-full bg-black px-4 py-2 text-csi-white dark:bg-csi-blue dark:text-csi-white"
+                >See Team</button
+            >
         </div>
     </div>
 </div>
