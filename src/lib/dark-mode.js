2b6d8103d191acdcfd9afa2c7098f886fa7c20e3
Eriene Galinato <erienegalinato@gmail.com>
2024-03-23 23:03:38 +0800

    chore: run formatters, linters, and builders

--- src/lib/dark-mode.js
+++ src/lib/dark-mode.js
@@ -2,16 +2,12 @@ import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
 /** @type {import('svelte/store').Writable<Boolean>} */
-export let darkMode = writable(
-    (browser)
-        ? window.matchMedia('(prefers-color-scheme: dark)').matches
-        : false
+export const darkMode = writable(
+    browser ? window.matchMedia('(prefers-color-scheme: dark)').matches : false,
 );
 
-darkMode.subscribe(($darkMode) => {
-    if (browser) {
-        ($darkMode)
-            ? document.documentElement.classList.add('dark')
-            : document.documentElement.classList.remove('dark');
-    }
+darkMode.subscribe($darkMode => {
+    if (browser)
+        if ($darkMode) document.documentElement.classList.add('dark');
+        else document.documentElement.classList.remove('dark');
 });
--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,15 +1,15 @@
 <script>
-    import upcsi from '$lib/icons/upcsi.svg';
-    import darkmode from '$lib/icons/darkmode.svg'
-    import lightmode from '$lib/icons/lightmode.svg'
     import { darkMode } from '$lib/dark-mode';
+    import darkmode from '$lib/icons/darkmode.svg';
+    import lightmode from '$lib/icons/lightmode.svg';
+    import upcsi from '$lib/icons/upcsi.svg';
 
     let mobileMenu = false;
 </script>
 
-<nav class="flex justify-center items-center">
+<nav class="flex items-center justify-center">
     <ul
-        class="flex flex-row items-center justify-between gap-4 bg-[#fcfdfd] dark:bg-[#0d0d0d] p-1 w-full lg:w-fit lg:rounded-xl"
+        class="flex w-full flex-row items-center justify-between gap-4 bg-[#fcfdfd] p-1 lg:w-fit lg:rounded-xl dark:bg-[#0d0d0d]"
     >
         <li class="mx-6 sm:mx-10 lg:ml-8 lg:mr-4">
             <img src={upcsi} alt="UP CSI Logo" class="h-8 object-contain" />
@@ -30,44 +30,102 @@
             <a href="/news/" class="text-csi-black dark:text-csi-white">Newsletter</a>
         </li>
         <li class="hidden lg:block">
-          <button class="flex justify-center items-center" on:click={() => { darkMode.update((dm) => dm = !dm) }}><img src={darkmode} alt="Toggle Dark Mode" class="h-6 mx-4 dark:hidden object-contain" /></button>
-          <button class="flex justify-center items-center" on:click={() => { darkMode.update((dm) => dm = !dm) }}><img src={lightmode} alt="Toggle Light Mode" class="h-6 mx-4 hidden dark:block object-contain" /></button>
+            <button
+                class="flex items-center justify-center"
+                on:click={() => {
+                    darkMode.update(dm => (dm = !dm));
+                }}
+                ><img
+                    src={darkmode}
+                    alt="Toggle Dark Mode"
+                    class="mx-4 h-6 object-contain dark:hidden"
+                /></button
+            >
+            <button
+                class="flex items-center justify-center"
+                on:click={() => {
+                    darkMode.update(dm => (dm = !dm));
+                }}
+                ><img
+                    src={lightmode}
+                    alt="Toggle Light Mode"
+                    class="mx-4 hidden h-6 object-contain dark:block"
+                /></button
+            >
         </li>
         <li class="hidden lg:block">
             <button class="rounded-lg bg-csi-blue px-5 py-2">Get in Touch</button>
         </li>
-        <li class="mx-6 sm:mx-10 my-2 lg:hidden flex flex-row items-center">
-            <button class="flex justify-center items-center" on:click={() => { darkMode.update((dm) => dm = !dm) }}><img src={darkmode} alt="Toggle Dark Mode" class="h-6 mx-4 dark:hidden object-contain" /></button>
-            <button class="flex justify-center items-center" on:click={() => { darkMode.update((dm) => dm = !dm) }}><img src={lightmode} alt="Toggle Light Mode" class="h-6 mx-4 hidden dark:block object-contain" /></button>
-            <button class="rounded-lg bg-black px-5 py-2 text-csi-white" on:click={() => mobileMenu = true}>Get Started</button>
+        <li class="mx-6 my-2 flex flex-row items-center sm:mx-10 lg:hidden">
+            <button
+                class="flex items-center justify-center"
+                on:click={() => {
+                    darkMode.update(dm => (dm = !dm));
+                }}
+                ><img
+                    src={darkmode}
+                    alt="Toggle Dark Mode"
+                    class="mx-4 h-6 object-contain dark:hidden"
+                /></button
+            >
+            <button
+                class="flex items-center justify-center"
+                on:click={() => {
+                    darkMode.update(dm => (dm = !dm));
+                }}
+                ><img
+                    src={lightmode}
+                    alt="Toggle Light Mode"
+                    class="mx-4 hidden h-6 object-contain dark:block"
+                /></button
+            >
+            <button
+                class="rounded-lg bg-black px-5 py-2 text-csi-white"
+                on:click={() => (mobileMenu = true)}>Get Started</button
+            >
         </li>
     </ul>
 </nav>
 
 {#if mobileMenu}
-    <div class="h-svh bg-blue-white flex flex-col">
+    <div class="flex h-svh flex-col bg-blue-white">
         <div class="fixed bottom-0 w-full">
             <ul class="w-full">
                 <li class="m-4">
-                    <a href="/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Home</a>
+                    <a href="/" class="font-dm text-2xl text-csi-black dark:text-csi-white">Home</a>
                 </li>
                 <li class="m-4">
-                    <a href="/about/" class="font-dm text-csi-black dark:text-csi-white text-2xl">About</a>
+                    <a href="/about/" class="font-dm text-2xl text-csi-black dark:text-csi-white"
+                        >About</a
+                    >
                 </li>
                 <li class="m-4">
-                    <a href="/events/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Events</a>
+                    <a href="/events/" class="font-dm text-2xl text-csi-black dark:text-csi-white"
+                        >Events</a
+                    >
                 </li>
                 <li class="m-4">
-                    <a href="/projects/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Projects</a>
+                    <a href="/projects/" class="font-dm text-2xl text-csi-black dark:text-csi-white"
+                        >Projects</a
+                    >
                 </li>
                 <li class="m-4">
-                    <a href="/news/" class="font-dm text-csi-black dark:text-csi-white text-2xl">Newsletter</a>
+                    <a href="/news/" class="font-dm text-2xl text-csi-black dark:text-csi-white"
+                        >Newsletter</a
+                    >
                 </li>
                 <li class="m-4">
-                    <button class="font-dm text-csi-black dark:text-csi-white text-2xl">Get in Touch</button>
+                    <button class="font-dm text-2xl text-csi-black dark:text-csi-white"
+                        >Get in Touch</button
+                    >
                 </li>
             </ul>
-            <div class="w-full flex justify-center items-center my-4"><button class="rounded-full bg-[#eff5f7] py-3 px-6" on:click={() => mobileMenu = false}>Close</button></div>
-        </div> 
+            <div class="my-4 flex w-full items-center justify-center">
+                <button
+                    class="rounded-full bg-[#eff5f7] px-6 py-3"
+                    on:click={() => (mobileMenu = false)}>Close</button
+                >
+            </div>
+        </div>
     </div>
 {/if}
