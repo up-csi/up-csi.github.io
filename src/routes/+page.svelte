62887840445dcfcff37811255c9608943ca52e00
Eriene Galinato <erienegalinato@gmail.com>
2024-03-23 23:19:42 +0800

    chore: run formatters, linters, and builders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 <section
     class="flex items-center bg-lino bg-contain bg-center bg-no-repeat bg-origin-content sm:h-[40vh] sm:bg-right lg:h-[60vh]"
     id="about"
-    >
+>
     <div class="prose">
         <h1 class="font-dm">Learn. Create. Innovate.</h1>
         <p>
