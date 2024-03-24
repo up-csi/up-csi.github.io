40d33510c66e19b50a7f99fe3c266384c36051dc
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-03-24 21:24:31 +0800

    chore: run formatter

--- src/routes/about/SectionHeaderTextContainer.svelte
+++ src/routes/about/SectionHeaderTextContainer.svelte
@@ -4,14 +4,20 @@
     /* TODO: implement the anchors for the buttons so that the page scrolls/jumps to those points when the buttons are clicked */
 </script>
 
-<section class="flex rounded-3xl w-full flex-col items-center gap-5 bg-white p-3 md:flex-row md:items-start dark:text-white dark:bg-csi-black">
+<section
+    class="flex w-full flex-col items-center gap-5 rounded-3xl bg-white p-3 md:flex-row md:items-start dark:bg-csi-black dark:text-white"
+>
     <!--PLACEHOLDER IMAGES-->
     <div class="w-full md:w-7/12">
         <img {src} alt="UP CSI Logo Placeholder" class="rounded-3xl bg-warm-white md:h-[500px]" />
     </div>
     <div class="flex flex-col gap-5 font-dm md:w-5/12">
         <div class="flex flex-col items-center md:items-start md:pb-6">
-            <img src={upcsi} alt="Circle placeholder" class="h-20 w-20 rounded-full bg-black p-2 dark:bg-csi-white" />
+            <img
+                src={upcsi}
+                alt="Circle placeholder"
+                class="h-20 w-20 rounded-full bg-black p-2 dark:bg-csi-white"
+            />
         </div>
         <h1 class="text-sm">Meet UP Center for Student Innovations</h1>
         <p class="text-3xl">
