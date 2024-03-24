eba0d542881c18fd617cd67f2aef7b10d157f62d
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-03-24 14:25:07 +0800

    feat: create +page.svelte for the about page and place SectionHeaderTextContainer inside

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import MissionVision from "./MissionVision.svelte";
+    import SectionHeaderTextContainer from "./SectionHeaderTextContainer.svelte";
+    import Contact from "$lib/components/Contact.svelte";
+
+</script>
+
+<section>
+    <SectionHeaderTextContainer />
+</section>
+<section>
+    <MissionVision />
+</section>
+<section>
+    <Contact />
+</section>
