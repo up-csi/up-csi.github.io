e0119de30094d1759aee15934d263bb2d6be733e
Eriene Galinato <erienegalinato@gmail.com>
2024-04-01 11:09:07 +0800

    chore(WhoWeAre.svelte): fix colors

--- src/routes/WhoWeAre.svelte
+++ src/routes/WhoWeAre.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <div class="flex flex-col items-center justify-center gap-12 font-dm md:flex-row">
-    <div class="flex w-full items-center justify-center rounded-3xl bg-csi-white md:w-2/5">
+    <div class="flex w-full items-center justify-center rounded-3xl md:w-2/5">
         <img class="w-4/5 md:w-full" {src} alt="Placeholder" />
     </div>
-    <div class="text- flex w-full flex-col gap-6 px-2 md:w-1/2 dark:text-csi-white">
+    <div class="flex w-full flex-col gap-6 px-2 text-csi-black md:w-1/2 dark:text-csi-white">
         <h1>Who we are</h1>
         <p class="text-xl md:text-2xl">
             We are a service-oriented organization aimed towards the enhancement of student learning
