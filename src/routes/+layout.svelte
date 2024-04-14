a569369703432119905121333b832585ac6d29fe
Ehren Castillo <ehrencastillo@gmail.com>
2024-04-14 15:07:36 +0800

    chore: run formatters, linters, and builders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 <header class="sticky top-0 z-50 lg:inset-x-0 lg:top-12">
     <NavBar />
 </header>
-<main class="m-4 space-y-16 sm:m-8 sm:mt-5 xl:mx-32 lg:my-16">
+<main class="m-4 space-y-16 sm:m-8 sm:mt-5 lg:my-16 xl:mx-32">
     <!-- m-4 space-y-16 sm:m-8 xl:m-32 xl:my-16 -->
     <slot />
 </main>
