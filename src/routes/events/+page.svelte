553055a9390aa61de97c876bae6383151cbdf2bd
Ehren Castillo <ehrencastillo@gmail.com>
2024-05-02 08:59:41 +0800

    feat: add EventCard component

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,3 +1,18 @@
+<script>
+    import EventCard from "./EventCard.svelte"; 
+
+    import imageSrc from "$lib/cartoon-card-placeholder.svg"
+</script>
+
 <article class="prose dark:prose-invert">
     <h1>Events</h1>
+
+    <EventCard imageSrc={imageSrc} pageUrl="/events/">
+        <svelte:fragment>
+            <p class="m-0">Upcoming - Internal</p>
+            <h2 class="m-0">ACENS1ON</h2>
+            <p class="m-0">August 4 - August 6, 2023</p>
+            <p class="m-0 overflow-hidden grow">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </svelte:fragment>
+    </EventCard>
 </article>
---
+++ src/routes/events/EventCard.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+    import type { HTMLImgAttributes } from 'svelte/elements';
+
+    export let imageSrc: HTMLImgAttributes['src'];
+    export let pageUrl: string;
+</script>
+
+<div class="flex flex-col w-72 h-[32rem] rounded-2xl overflow-hidden shadow-lg bg-csi-neutral-50 dark:bg-csi-neutral-900">
+    <img src={imageSrc} alt="Corporate Cartoon" class="h-56 object-cover m-0"/>
+    <div class="flex flex-col gap-2 h-full justify-between overflow-hidden m-3">
+        <slot />
+        <p class="m-0"><a href={pageUrl}>More Info</a></p>
+    </div>
+</div>
