429f0703826fe84ddcc1f73f6f56b5b6105be28d
Ehren Castillo <ehrencastillo@gmail.com>
2024-05-04 01:39:10 +0800

    feat: add loop for generating event cards

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,18 +1,24 @@
 <script>
-    import EventCard from "./EventCard.svelte"; 
+    import EventCard from "./EventCard.svelte";
 
-    import imageSrc from "$lib/cartoon-card-placeholder.svg"
+    import events from "./events.json"
 </script>
 
-<article class="prose dark:prose-invert">
+<article class="prose dark:prose-invert max-w-full">
     <h1>Events</h1>
 
-    <EventCard imageSrc={imageSrc} pageUrl="/events/">
-        <svelte:fragment>
-            <p class="m-0">Upcoming - Internal</p>
-            <h2 class="m-0">ACENS1ON</h2>
-            <p class="m-0">August 4 - August 6, 2023</p>
-            <p class="m-0 overflow-hidden grow">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </svelte:fragment>
-    </EventCard>
+    <div class="flex justify-center">
+        <div class="grid w-fit items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {#each events as event}
+            <EventCard src={event.src} alt={`Image of ${event.title}`} href={event.url}>
+            <svelte:fragment>
+                <p class="m-0">{event.state} - {event.type}</p>
+                <h2 class="m-0">{event.title}</h2>
+                <p class="m-0">{event.dates}</p>
+                <p class="m-0 overflow-hidden grow">{event.description}</p>
+            </svelte:fragment>
+            </EventCard>
+            {/each}
+        </div>
+    </div>
 </article>
--- src/routes/events/EventCard.svelte
+++ src/routes/events/EventCard.svelte
@@ -1,14 +1,13 @@
 <script lang='ts'>
-    import type { HTMLImgAttributes } from 'svelte/elements';
-
-    export let imageSrc: HTMLImgAttributes['src'];
-    export let pageUrl: string;
+    export let src = '';
+    export let alt = '';
+    export let href = '';
 </script>
 
 <div class="flex flex-col w-72 h-[32rem] rounded-2xl overflow-hidden shadow-lg bg-csi-neutral-50 dark:bg-csi-neutral-900">
-    <img src={imageSrc} alt="Corporate Cartoon" class="h-56 object-cover m-0"/>
+    <img {src} {alt} class="h-56 object-cover m-0"/>
     <div class="flex flex-col gap-2 h-full justify-between overflow-hidden m-3">
         <slot />
-        <p class="m-0"><a href={pageUrl}>More Info</a></p>
+        <p class="m-0"><a {href}>More Info</a></p>
     </div>
 </div>
