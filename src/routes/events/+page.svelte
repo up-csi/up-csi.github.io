0dd4838e19605715f79afbb0da49585b3c79e7aa
Ehren Castillo <ehrencastillo@gmail.com>
2024-05-04 01:47:22 +0800

    feat: move card loading to EventsPanel

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,24 +1,9 @@
 <script>
-    import EventCard from "./EventCard.svelte";
-
-    import events from "./events.json"
+    import EventsPanel from "./EventsPanel.svelte";
 </script>
 
 <article class="prose dark:prose-invert max-w-full">
     <h1>Events</h1>
 
-    <div class="flex justify-center">
-        <div class="grid w-fit items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {#each events as event}
-            <EventCard src={event.src} alt={`Image of ${event.title}`} href={event.url}>
-            <svelte:fragment>
-                <p class="m-0">{event.state} - {event.type}</p>
-                <h2 class="m-0">{event.title}</h2>
-                <p class="m-0">{event.dates}</p>
-                <p class="m-0 overflow-hidden grow">{event.description}</p>
-            </svelte:fragment>
-            </EventCard>
-            {/each}
-        </div>
-    </div>
+    <EventsPanel />
 </article>
---
+++ src/routes/events/EventsPanel.svelte
@@ -0,0 +1,22 @@
+<script>
+    import EventCard from "./EventCard.svelte";
+
+    import events from "./events.json"
+</script>
+
+<div>
+    <div class="flex justify-center">
+        <div class="grid w-fit items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {#each events as event}
+            <EventCard src={event.src} alt={`Image of ${event.title}`} href={event.url}>
+                <svelte:fragment>
+                    <p class="m-0">{event.state} - {event.type}</p>
+                    <h2 class="m-0">{event.title}</h2>
+                    <p class="m-0">{event.dates}</p>
+                    <p class="m-0 overflow-hidden grow">{event.description}</p>
+                </svelte:fragment>
+            </EventCard>
+            {/each}
+        </div>
+    </div>
+</div>
