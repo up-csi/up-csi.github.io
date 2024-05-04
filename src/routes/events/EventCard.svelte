525e74e553aaed0b5987e4f1bfd292c51548b2ac
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-05-04 15:44:38 +0800

    perf: lazy-load images in events

--- src/routes/events/EventCard.svelte
+++ src/routes/events/EventCard.svelte
@@ -10,7 +10,7 @@
 <div
     class="flex h-auto w-full flex-col overflow-hidden rounded-2xl bg-csi-neutral-50 shadow-lg md:h-[32rem] md:w-72 dark:bg-csi-neutral-900"
 >
-    <img {src} {alt} class="m-0 h-64 shrink-0 object-cover md:h-48" />
+    <img {src} {alt} loading="lazy" class="m-0 h-64 shrink-0 object-cover md:h-48" />
     <div class="m-3 flex h-full flex-col justify-between gap-2 overflow-hidden">
         <slot />
         <p class="m-0"><a {href}>More Info</a></p>
