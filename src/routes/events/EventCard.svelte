bf39de2d11572681fa149ab888d75114ac65053a
Ehren Castillo <ehrencastillo@gmail.com>
2024-05-04 13:18:54 +0800

    feat(EventCard.svelte): add more responsive styles for small screens

--- src/routes/events/EventCard.svelte
+++ src/routes/events/EventCard.svelte
@@ -4,8 +4,8 @@
     export let href: string;
 </script>
 
-<div class="flex flex-col w-72 h-[32rem] rounded-2xl overflow-hidden shadow-lg bg-csi-neutral-50 dark:bg-csi-neutral-900">
-    <img {src} {alt} class="h-48 object-cover m-0 shrink-0"/>
+<div class="flex flex-col w-full md:w-72 h-auto md:h-[32rem] rounded-2xl overflow-hidden shadow-lg bg-csi-neutral-50 dark:bg-csi-neutral-900">
+    <img {src} {alt} class="h-64 md:h-48 object-cover m-0 shrink-0"/>
     <div class="flex flex-col gap-2 h-full justify-between overflow-hidden m-3">
         <slot />
         <p class="m-0"><a {href}>More Info</a></p>
