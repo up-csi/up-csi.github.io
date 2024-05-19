5855641273bdb82cb5ca79a8809305d793e25a7d
Eriene Galinato <edgalinato@up.edu.ph>
2024-05-19 20:16:15 +0800

    feat: add unexpected error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { page } from '$app/stores';
+    import Err404 from '$lib/components/error/Err404.svelte'
+</script>
+
+{#if $page.error}
+    {#if $page.status === 404}
+        <Err404 />
+    {:else}
+        <h1>{$page.status}</h1>
+        <h1>{$page.error.message}</h1>
+    {/if}
+{:else}
+    <h1>{$page.status}</h1>
+    <h1>Woops! An error occurred.</h1>
+{/if}
