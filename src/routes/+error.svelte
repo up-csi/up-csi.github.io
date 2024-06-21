69d1920abf582b343806de8e5966ca7ff945b79b
Eriene Galinato <edgalinato@up.edu.ph>
2024-06-21 12:40:07 +0800

    feat(+error.svelte): add dark mode Tailwind css

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,10 +7,10 @@
     {#if $page.status === 404}
         <Err404 />
     {:else}
-        <p>{$page.status}</p>
-        <p>{$page.error.message}</p>
+        <p class="my-12 text-center text-csi-black dark:text-csi-white">{$page.status}</p>
+        <p class="my-12 text-center text-csi-black dark:text-csi-white">{$page.error.message}</p>
     {/if}
 {:else}
-    <p>{$page.status}</p>
-    <p>Woops! An error occurred.</p>
+    <p class="my-12 text-center text-csi-black dark:text-csi-white">{$page.status}</p>
+    <p class="my-12 text-center text-csi-black dark:text-csi-white">Woops! An error occurred.</p>
 {/if}
