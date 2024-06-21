5acaded707d53ee2c736601aa0a4ae6dc72de5a9
Eriene Galinato <edgalinato@up.edu.ph>
2024-06-21 11:58:38 +0800

    feat: add 404 custom error page

---
+++ src/lib/components/error/Err404.svelte
@@ -0,0 +1,10 @@
+<script>
+    import thonk_lino from '$lib/images/thonk_lino.png';
+</script>
+
+<section class="my-44 w-full h-full">
+    <div class="m-0 w-full flex justify-center items-center">
+        <img src={thonk_lino} alt="Thinking Lino Doodle" />
+    </div>
+    <h1 class="my-12 text-center text-csi-black dark:text-csi-white">Woops! Looks like this page isn't available. Go back <a href="/" class="underline">home</a>?</h1>
+</section>
