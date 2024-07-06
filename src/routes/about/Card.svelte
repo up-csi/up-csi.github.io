efd8fd0dbd6beba833a6aad49a1484d2e5ab0c50
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-07-06 11:13:50 +0800

    feat: replace MissionVision placeholder images with cartoons

--- src/routes/about/Card.svelte
+++ src/routes/about/Card.svelte
@@ -1,10 +1,13 @@
 <script>
-    import src from '$lib/lino-sablay.svg';
+    export let src;
+    export let bgStyles = 'bg-csi-neutral-200 rounded-t-full rounded-b-xl';
 </script>
 
 <div class="flex w-full flex-col gap-2 md:w-1/4">
-    <img {src} alt="Placeholder" class="w-full" />
-    <div class="w-full">
+    <div class={`flex w-full justify-center items-center h-3/5 ${bgStyles}`}>
+        <img {src} alt="Placeholder" />
+    </div>
+    <div class="w-full h-2/5">
         <h1 class="text-2xl">
             <slot name="heading" />
         </h1>
