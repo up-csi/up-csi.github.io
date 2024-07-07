05187a5bc3d6683d97702a72c7073f710bce7245
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-07-07 11:02:48 +0800

    feat: replace Home page images with props

--- src/lib/components/CartoonCard.svelte
+++ src/lib/components/CartoonCard.svelte
@@ -1,13 +1,15 @@
 <script>
-    import src from '$lib/cartoon-card-placeholder.svg';
+    export let src;
 </script>
 
-<div class="mb-8 flex w-3/5 flex-col items-center lg:mb-0 lg:w-2/5 xl:w-3/5">
-    <div class="w-full rounded-b-[50px] rounded-t-[180px] bg-csi-blue px-2 pt-5">
+<div class="mb-8 flex flex-col md:mb-0 md:w-2/5 xl:w-3/5 h-full">
+    <div class="flex aspect-square overflow-y-hidden items-center rounded-3xl bg-csi-blue px-2 pt-5">
         <img {src} alt="Corporate Cartoon" />
     </div>
-    <div class="leading-6">
-        <h1 class="text-center font-dm text-3xl"><slot name="title" /></h1>
-        <p class="text-center text-csi-black dark:text-csi-white"><slot /></p>
-    </div>
+    {#if $$slots.title || $$slots.default}
+        <div class="leading-6">
+            <h1 class="text-center font-dm text-3xl"><slot name="title" /></h1>
+            <p class="text-center text-csi-black dark:text-csi-white"><slot /></p>
+        </div>
+    {/if}
 </div>
--- src/lib/components/ContentContainer.svelte
+++ src/lib/components/ContentContainer.svelte
@@ -1,11 +1,11 @@
 <div
-    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-csi-neutral-50 px-8 py-8 md:px-32 md:py-16 xl:flex-row dark:bg-csi-neutral-950"
+    class="flex flex-col items-center justify-between gap-12 rounded-3xl bg-csi-neutral-50 px-8 py-8 md:px-24 md:py-16 xl:flex-row dark:bg-csi-neutral-950"
 >
     <div class="flex flex-1 flex-col gap-4">
         <slot name="text" />
     </div>
     <div
-        class="flex w-full flex-col items-center justify-center lg:flex-row lg:gap-12 xl:h-full xl:w-1/2 xl:items-start xl:justify-between xl:gap-4"
+        class="flex w-5/6 justify-center gap-4 flex-col md:flex-row xl:w-1/2"
     >
         <slot name="cards" />
     </div>
