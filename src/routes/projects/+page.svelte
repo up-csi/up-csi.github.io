ca52f7ee5ae71293b9c0e3175ad5f9517e431047
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-09-06 21:22:11 +0800

    feat: move events and projects to .js files

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,23 +1,10 @@
 <script>
+    import projects from './projects.js';
     import ProjectsPanel from '$lib/components/EventsPanel.svelte';
-    import lino from '$lib/lino-sablay.svg';
 
     const filters = ['All', 'Service', 'Innovation', 'Other'];
-
-    const events = [
-        {
-            tag: 'Service',
-            state: 'Past',
-            type: 'Internal',
-            title: 'Service Project',
-            dates: 'August 4 - August 6, 2023',
-            description: 'Lorep ipsum.',
-            src: lino,
-            url: '/projects/',
-        },
-    ];
 </script>
 
 <article class="prose max-w-full dark:prose-invert">
-    <ProjectsPanel {filters} {events} />
+    <ProjectsPanel {filters} events={projects} />
 </article>
---
+++ src/routes/projects/projects.js
@@ -0,0 +1,14 @@
+import lino from '$lib/lino-sablay.svg';
+
+export default [
+    {
+        tag: 'Service',
+        state: 'Past',
+        type: 'Internal',
+        title: 'Service Project',
+        dates: 'August 4 - August 6, 2023',
+        description: 'Lorep ipsum.',
+        src: lino,
+        url: '/projects/',
+    },
+];
