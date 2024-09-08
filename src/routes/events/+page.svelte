365d99f75134d0d54e0f6125c00e5b350f1a89a9
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-08 18:04:30 +0800

    feat(Events): add featured events component to page with placeholder information

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import EventsPanel from '$lib/components/EventsPanel.svelte';
+    import lino from '$lib/lino-sablay.svg';
     import events from './events';
+    import FeaturedEvent from './FeaturedEvent.svelte';
 
     const filters = [
         'All',
@@ -13,6 +15,11 @@
     ];
 </script>
 
-<article class="prose max-w-full dark:prose-invert">
+<article class="prose flex max-w-full flex-col gap-8 dark:prose-invert">
+    <FeaturedEvent tags={['App Process']} schedule={['']}>
+        <img slot="image" src={lino} alt="Incoming App Process..." />
+        <svelte:fragment slot="name">Incoming App Process</svelte:fragment>
+        <svelte:fragment slot="description">Stay tuned...</svelte:fragment>
+    </FeaturedEvent>
     <EventsPanel {filters} {events} />
 </article>
