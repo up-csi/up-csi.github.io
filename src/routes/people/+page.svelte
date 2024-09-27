5eacaa5d565919e1fe0191111a1b8404af1c51e2
Eriene Galinato <edgalinato@up.edu.ph>
2024-09-27 17:21:17 +0800

    feat: add dropdown for .exe

--- src/routes/people/+page.svelte
+++ src/routes/people/+page.svelte
@@ -12,25 +12,34 @@
     import eyronTanael from '$lib/people/eyron-tanael.webp';
     import hansLorico from '$lib/people/hans-lorico.webp';
     import jellyRaborar from '$lib/people/jelly-raborar.webp';
+
+    let pastExec = false;
 </script>
 
-<article class="prose flex max-w-none flex-col gap-8">
-    <figure class="m-0 flex flex-col items-center">
-        <picture class="max-w-3xl">
-            <source type="image/webp" srcset={execSmall} media="(max-width:640px)" />
-            <source type="image/webp" srcset={execMedium} />
-            <img src={execFull} width="1568" height="1203" alt="The Executive Board" />
-        </picture>
-        <figcaption>The 2023-2024 Executive Board</figcaption>
-    </figure>
-    <section class="not-prose flex flex-wrap justify-around gap-8">
-        <Exec src={calebBunye} alt="Caleb Bunye" pos="President" />
-        <Exec src={hansLorico} alt="Hans Lorico" pos="Vice President for Innovation" />
-        <Exec src={eyronTanael} alt="Eyron Tanael" pos="Vice President for Service" />
-        <Exec src={bastiOrtiz} alt="Basti Ortiz" pos="Director for Engineering" />
-        <Exec src={arianRey} alt="Arian Rey" pos="Director for Membership & Internals" />
-        <Exec src={jellyRaborar} alt="Jelly Raborar" pos="Director for Branding & Creatives" />
-        <Exec src={beaDiokno} alt="Bea Diokno" pos="Director for External Relations" />
-    </section>
+<section>
     <TeamPanel />
-</article>
+</section>
+<section>
+    <button on:click={() => { pastExec = !pastExec; }}>2023-2024</button>
+    {#if pastExec}
+        <section class="prose flex max-w-none flex-col gap-8">
+            <figure class="m-0 flex flex-col items-center">
+                <picture class="max-w-3xl">
+                    <source type="image/webp" srcset={execSmall} media="(max-width:640px)" />
+                    <source type="image/webp" srcset={execMedium} />
+                    <img src={execFull} width="1568" height="1203" alt="The Executive Board" />
+                </picture>
+                <figcaption>The 2023-2024 Executive Board</figcaption>
+            </figure>
+            <section class="not-prose flex flex-wrap justify-around gap-8">
+                <Exec src={calebBunye} alt="Caleb Bunye" pos="President" />
+                <Exec src={hansLorico} alt="Hans Lorico" pos="Vice President for Innovation" />
+                <Exec src={eyronTanael} alt="Eyron Tanael" pos="Vice President for Service" />
+                <Exec src={bastiOrtiz} alt="Basti Ortiz" pos="Director for Engineering" />
+                <Exec src={arianRey} alt="Arian Rey" pos="Director for Membership & Internals" />
+                <Exec src={jellyRaborar} alt="Jelly Raborar" pos="Director for Branding & Creatives" />
+                <Exec src={beaDiokno} alt="Bea Diokno" pos="Director for External Relations" />
+            </section>
+        </section>
+    {/if}
+</section>
