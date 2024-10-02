d552b4c09ccef1f7527b65562b974d4d5af6d518
Ehren Castillo <ehrencastillo@gmail.com>
2024-10-02 18:38:36 +0800

    feat: write a possible way to go about generalization for reference

--- src/lib/projects/projects.ts
+++ src/lib/projects/projects.ts
@@ -1,15 +1,21 @@
-import type { Event } from '$lib/components/EventsPanel.svelte';
 import lino from '$lib/lino-sablay.svg';
+
+export interface Project {
+    tag: string;
+    type: string;
+    title: string;
+    description: string;
+    src: string;
+    url: string;
+}
 
 export default [
     {
         tag: 'Service',
-        state: 'Past',
         type: 'Internal',
         title: 'Service Project',
-        dates: 'August 4 - August 6, 2023',
         description: 'Lorep ipsum.',
         src: lino,
         url: '/projects/',
     },
-] satisfies Event[];
+] satisfies Project[];
