ab4e32f95b6129d43fde54dcefd93c71034f1a7b
Ehren Castillo <ehrencastillo@gmail.com>
2024-10-14 22:38:09 +0800

    refactor(team): prefer `basename` from `node:path` over RegEx

--- src/lib/people/team/team.ts
+++ src/lib/people/team/team.ts
@@ -1,15 +1,13 @@
 import { type Member, Member as MemberSchema } from '$lib/models/member';
+import { basename } from 'node:path';
 import { parse } from 'valibot';
-
-const baseRe = /(?<base>\w+).json$/;
 
 export async function getTeam() {
     const imports = import.meta.glob<Member>('./members/*.json');
 
     const promises = Object.entries(imports).map(async ([path, asset]) => {
         // Derive the image name from the original path
-        // It goes without saying that this may not be the right approach for this
-        const base = path.match(baseRe)?.groups?.base;
+        const base = basename(path, '.json');
         const src: string = (await import(`$lib/people/team/images/${base}.webp?url`)).default;
 
         const member = parse(MemberSchema, await asset());
