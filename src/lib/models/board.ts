623f14a9888e686bc0b528a53049696bab900936
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-07 09:43:24 +0800

    feat(Exec): define board model

---
+++ src/lib/models/board.ts
@@ -0,0 +1,24 @@
+import * as v from 'valibot';
+
+export const POSITIONS = [
+    'President',
+    'Vice President for Innovation',
+    'Vice President for Service',
+    'Director for External Relations',
+    'Director for Membership & Internals',
+    'Director for Branding & Creatives',
+    'Director for Engineering',
+] as const;
+
+
+export interface Board extends v.InferOutput<typeof Board> {
+    src: string;
+}
+
+export const Board = v.object({
+    year: v.string(),
+    board: v.array(v.record(
+        v.string(),
+        v.picklist(POSITIONS),
+    )),
+});
