29fe2be7b9d966ee8b08e13932cd48d4deb7b6f2
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-07 17:55:14 +0800

    fix: reverse exec data array

--- src/routes/people/+page.server.ts
+++ src/routes/people/+page.server.ts
@@ -2,5 +2,5 @@ import { getExec } from '$lib/people/exec/exec';
 import { getTeam } from '$lib/people/team/team';
 
 export async function load() {
-    return { team: await getTeam(), exec: await getExec() };
+    return { team: await getTeam(), exec: (await getExec()).reverse() };
 }
