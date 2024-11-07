239379fe0abf6a485300da5cef962391e08423c6
Eriene Galinato <99855294+galierie@users.noreply.github.com>
2024-11-07 17:29:39 +0800

    fix(Exec): add function to parse officer data

--- src/lib/people/exec/exec.ts
+++ src/lib/people/exec/exec.ts
@@ -1,9 +1,26 @@
 import { type Board, Board as BoardSchema } from '$lib/models/board';
 import type { EnhancedImgAttributes } from '@sveltejs/enhanced-img';
+import { type InferOutput, parse } from 'valibot';
 import { Officer as OfficerSchema } from '$lib/models/officer';
 
 import { basename } from 'node:path';
-import { parse } from 'valibot';
+
+async function getOfficers(data: InferOutput<typeof OfficerSchema>[], folder: string) {
+    const promises = data.map(async (datum) => {
+        const person = parse(OfficerSchema, datum)
+        const img = person.name.replace(/\s+/g, '-').toLowerCase();
+
+        try {
+            const src: string | null = (await import(`$lib/people/exec/images/${folder}/${img}.webp?url`)).default;
+            return { ...person, src };
+        } catch {
+            const src: string | null = null;
+            return { ...person, src };
+        }
+    });
+
+    return await Promise.all(promises);
+}
 
 export async function getExec() {
     const imports = import.meta.glob<Board>('./boards/*.json');
@@ -13,18 +30,7 @@ export async function getExec() {
         const board = parse(BoardSchema, await asset());
         const { year, officers: data } = board;
 
-        const officers = data.map(async (datum) => {
-            const person = parse(OfficerSchema, datum)
-            const personImg = person.name.replace(/\s+/g, '-').toLowerCase();
-
-            try {
-                const personSrc: string | null = (await import(`$lib/people/exec/images/${acadYear}/${personImg}.webp?url`)).default;
-                return { ...person, personSrc };
-            } catch {
-                const personSrc: string | null = null;
-                return { ...person, personSrc };
-            }
-        });
+        const officers = await getOfficers(data, acadYear);
 
         try {
             const src: EnhancedImgAttributes['src'] | null = (await import(`$lib/people/exec/images/${acadYear}/${acadYear}.webp?enhanced?url`)).default;
