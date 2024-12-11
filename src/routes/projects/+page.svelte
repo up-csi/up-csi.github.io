fa4fd2537441292fc84e69792866350b4ac833d8
s0mme <hanz.vicencio+github@gmail.com>
2024-12-11 11:20:41 +0800

    feat: add FAQs section and dropdown component to Projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Accordion from '$lib/components/Accordion.svelte';
     import FilterPagePanel from '$lib/components/FilterPagePanel.svelte';
     import GeneralInfo from './GeneralInfo.svelte';
     import SocialMediaPanel from '$lib/components/SocialMediaPanel.svelte';
@@ -16,3 +17,10 @@
         <SocialMediaPanel />
     </div>
 </article>
+
+<section>
+    <h1 class="mb-4 w-2/5 text-3xl text-csi-black md:text-4xl dark:text-csi-white">FAQs</h1>
+    <Accordion title="Lorem ipsum dolor sit amet?">
+        <span> Lorem ipsum dolor sit amet adipiscing elit consectitur </span>
+    </Accordion>
+</section>
