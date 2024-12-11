d608f57682f00cff26018cedc6170c76ef0bdd44
s0mme <hanz.vicencio+github@gmail.com>
2024-12-11 11:20:03 +0800

    feat(Accordion): make dropdown component for Projects page (#102)

---
+++ src/lib/components/Accordion.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import ChevronDown from '@iconify/icons-heroicons/chevron-down';
+    import Icon from '@iconify/svelte';
+    import type { Snippet } from 'svelte';
+    import { slide } from 'svelte/transition';
+
+    interface Props {
+        title: string;
+        children?: Snippet;
+    }
+
+    let open: boolean = $state(false);
+
+    function toggle() {
+        open = !open;
+    }
+
+    const { children, title }: Props = $props();
+</script>
+
+<div class="w-full rounded-2xl shadow-lg">
+    <button
+        class="flex w-full rounded-2xl bg-csi-neutral-50 px-5 py-4 aria-expanded:bg-csi-neutral-100"
+        onclick={toggle}
+        aria-expanded={open}
+    >
+        <span class="grow text-left font-inter text-lg font-bold">{title}</span>
+        <span class="grow-0">
+            <Icon
+                icon={ChevronDown}
+                class="size-5 text-csi-black transition-transform aria-expanded:rotate-90 dark:text-csi-white"
+                aria-expanded={open}
+            />
+        </span>
+    </button>
+    {#if open}
+        <div class="flex w-full items-center px-5 py-4" transition:slide>
+            {@render children?.()}
+        </div>
+    {/if}
+</div>
