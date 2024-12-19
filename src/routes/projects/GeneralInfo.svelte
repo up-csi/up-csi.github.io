c2929c48d4d6ecc6e1c20b7107dbe6bfabdc8091
Nodeil <rivercelest7@gmail.com>
2024-12-19 23:59:12 +0800

    fix: fix some prettier and linter errors

--- src/routes/projects/GeneralInfo.svelte
+++ src/routes/projects/GeneralInfo.svelte
@@ -1,14 +1,16 @@
-<div class="m-0 rounded-2xl bg-csi-neutral-50 px-8 py-8 font-inter text-csi-black shadow-lg lg:max-w-fit dark:bg-csi-neutral-900">
+<div
+    class="m-0 rounded-2xl bg-csi-neutral-50 px-8 py-8 font-inter text-csi-black shadow-lg lg:max-w-fit dark:bg-csi-neutral-900"
+>
     <div class="flex flex-col gap-5">
         <div>
-            <h1 class="m-0"> General Info </h1>
+            <h1 class="m-0">General Info</h1>
         </div>
 
-        <div class="w-full justify-center p-0.5 border-csi-blue bg-csi-blue"></div>
+        <div class="w-full justify-center border-csi-blue bg-csi-blue p-0.5"></div>
 
         <div>
-            <p class="m-0 lg:max-w-80 max-w-full">
-                lino@up-csi.org <br/>
+            <p class="m-0 max-w-full lg:max-w-80">
+                lino@up-csi.org <br />
                 Department of Computer Science, UP Diliman, Diliman, Quezon City, Metro Manila
             </p>
         </div>
