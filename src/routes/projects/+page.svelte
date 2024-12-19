eeb84fd524ccfeab05ff92f16accaa887887d28c
Aeron Penaflorida <aeronpenaflorida@gmail.com>
2024-12-19 23:24:52 +0800

    feat(projects/+page.svelte): import FAQ.svelte

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import FilterPagePanel from '$lib/components/FilterPagePanel.svelte';
     import projects from '$lib/projects/projects';
+    import Faq from '$lib/components/FAQ.svelte';
     const filters = ['All', 'Service', 'Innovation', 'Other'];
 </script>
 
