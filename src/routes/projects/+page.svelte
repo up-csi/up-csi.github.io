279deccbfa40edb988fdce2f7bfb2fe2a7f859a8
Nodeil <rivercelest7@gmail.com>
2024-12-19 23:55:35 +0800

    feat(GeneralInfo.svelte): add general info tile

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,9 +1,15 @@
 <script>
     import FilterPagePanel from '$lib/components/FilterPagePanel.svelte';
     import projects from '$lib/projects/projects';
+    import GeneralInfo from './GeneralInfo.svelte';
     const filters = ['All', 'Service', 'Innovation', 'Other'];
 </script>
 
-<article class="prose max-w-full dark:prose-invert">
-    <FilterPagePanel {filters} cardsInfo={projects} />
+<article class="prose max-w-full flex flex-row gap-4 lg:flex-row flex-col dark:prose-invert">
+    <div class="grow">
+        <FilterPagePanel {filters} cardsInfo={projects} />
+    </div>
+    <div>
+        <GeneralInfo />
+    </div>
 </article>
