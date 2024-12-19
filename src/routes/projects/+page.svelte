bd185383fefe75b0dbd112dbe8c5d5d451d747d5
Aeron Penaflorida <aeronpenaflorida@gmail.com>
2024-12-19 23:32:07 +0800

    feat(projects/+page.svelte): add paragraph to Faq

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,4 +8,6 @@
 <article class="prose max-w-full dark:prose-invert">
     <FilterPagePanel {filters} cardsInfo={projects} />
 </article>
-<Faq faq_title="GENERAL FAQ"></Faq>
+<Faq faq_title="GENERAL FAQ"  
+     paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+></Faq>
