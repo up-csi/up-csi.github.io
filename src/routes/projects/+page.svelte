a84253d53e200993a7a6b5a16fc84eec76db1be0
Nodeil <rivercelest7@gmail.com>
2024-12-19 23:59:12 +0800

    fix: fix some prettier and linter errors

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,11 @@
 <script>
     import FilterPagePanel from '$lib/components/FilterPagePanel.svelte';
-    import projects from '$lib/projects/projects';
     import GeneralInfo from './GeneralInfo.svelte';
+    import projects from '$lib/projects/projects';
     const filters = ['All', 'Service', 'Innovation', 'Other'];
 </script>
 
-<article class="prose max-w-full flex flex-row gap-4 lg:flex-row flex-col dark:prose-invert">
+<article class="prose flex max-w-full flex-row flex-col gap-4 dark:prose-invert lg:flex-row">
     <div class="grow">
         <FilterPagePanel {filters} cardsInfo={projects} />
     </div>
