02c5a1e2657fc0b03e67c85d20045f9739132cac
Aeron Penaflorida <aeronpenaflorida@gmail.com>
2024-12-19 23:33:50 +0800

    feat(projects/+page.svelte): add second paragraph to Faq

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,4 +11,5 @@
 <Faq faq_title="GENERAL FAQ"  
      paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
      faq_title1="GENERAL FAQ"
+     paragraph1="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 ></Faq>
