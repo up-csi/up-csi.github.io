df677eceb57e844f1a9b961ce3fa0d73104afe0c
Aeron Penaflorida <aeronpenaflorida@gmail.com>
2024-12-19 23:17:41 +0800

    feat(FAQ.svelte): add styled paragraph and paragraph1 to text container

--- src/lib/components/FAQ.svelte
+++ src/lib/components/FAQ.svelte
@@ -11,8 +11,14 @@
 <div class="container mx-auto px-20 flex flex-col md:flex-row md:justify-between items-center gap-8">
     <div class="text-center max-w-md md:w-1/2 dark:hidden">
         <h2 class="font-lato font-bold mb-2 text-xl md:text-lg">{faq_title}</h2>
+        <p class="font-lato text-sm md:text-base text-gray-700 leading-relaxed text-justify">
+            {paragraph}
+          </p>
     </div>
     <div class="text-center max-w-md md:w-1/2 dark:hidden">
         <h2 class="font-lato font-bold mb-2 text-xl md:text-lg">{faq_title1}</h2>
+        <p class="font-lato text-sm md:text-base text-gray-700 leading-relaxed text-justify">
+            {paragraph}
+          </p>
     </div>
 </div>
