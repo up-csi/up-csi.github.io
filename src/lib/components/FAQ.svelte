492f038ddd75f199486ad48d6eb036fa861b0c3d
Aeron Penaflorida <aeronpenaflorida@gmail.com>
2024-12-19 22:18:43 +0800

    feat(FAQ.svelte): initialize props

---
+++ src/lib/components/FAQ.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    interface Props{
+        faq_title:string;
+        paragraph:string;
+        faq_title1:string;
+        paragraph1:string;
+    }
+    const {faq_title, paragraph, faq_title1, paragraph1}:Props = $props();
+</script>
