efceb4be02e24552fad5ed8d8702e00811166e4b
rue-22 <cssalces@up.edu.ph>
2024-12-28 10:51:02 +0800

    feat: make text justified and card heights constant

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <div
-    class="flex h-auto w-full flex-col overflow-hidden rounded-2xl bg-csi-neutral-50 px-1 shadow-lg md:max-w-72 md:px-2 dark:bg-csi-neutral-900"
+    class="flex h-full w-full flex-col overflow-hidden rounded-2xl bg-csi-neutral-50 px-1 shadow-lg md:max-w-72 md:px-2 dark:bg-csi-neutral-900"
 >
     {#if src}
         <img {src} alt={project} loading="lazy" class="m-0 h-64 shrink-0 object-cover md:h-48" />
     {/if}
     <div class="md:text-md m-3 flex h-full flex-col justify-between gap-2 overflow-hidden">
         <h2 class="m-0">{project}</h2>
-        <p class="m-0 grow overflow-hidden">{description}</p>
+        <p class="m-0 grow overflow-hidden text-justify">{description}</p>
     </div>
 </div>
