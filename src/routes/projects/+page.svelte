8a089b7b5fa4d116a1f5c7049d27c5eefbc2dd73
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-23 06:11:20 +0800

    fix: make use of refactored components in Projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,24 +1,28 @@
 <script>
     import Faq from '$lib/components/Faq.svelte';
+    import FaqEntry from '$lib/components/FaqEntry.svelte';
     import FilterPagePanel from '$lib/components/FilterPagePanel.svelte';
     import projects from '$lib/projects/projects';
     const filters = ['All', 'Service', 'Innovation', 'Other'];
-    const faqEntries = [
-        {title: "GENERAL FAQ", paragraph: faqParagraph1},
-        {title: "GENERAL FAQ", paragraph: faqParagraph2}
-    ]
 </script>
 
 <article class="prose max-w-full dark:prose-invert">
     <FilterPagePanel {filters} cardsInfo={projects} />
 </article>
 
-{#snippet faqParagraph1()}
-    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
-{/snippet}
-
-{#snippet faqParagraph2()}
-    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
-{/snippet}
-
-<Faq {faqEntries}/>
+<Faq>
+    <FaqEntry title="GENERAL FAQ">
+        <span
+            >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span
+        >
+    </FaqEntry>
+    <FaqEntry title="GENERAL FAQ">
+        <span
+            >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span
+        >
+    </FaqEntry>
+</Faq>
