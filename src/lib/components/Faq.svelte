93f3a7489f6265b17c12fbf3f35ed5688f5b4b90
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-23 06:10:23 +0800

    refactor!: prefer using $props.children for populating Faq and FaqEntry

--- src/lib/components/Faq.svelte
+++ src/lib/components/Faq.svelte
@@ -1,26 +1,15 @@
+<!-- Faq.svelte -->
 <script lang="ts">
-    import type { Snippet } from "svelte";
+    import type { Snippet } from 'svelte';
 
-    export interface FaqEntry {
-        title: string;
-        paragraph: Snippet;
-    }
-    
     interface Props {
-        faqEntries: FaqEntry[]
+        children?: Snippet;
     }
-    const { faqEntries }: Props = $props();
+    const { children }: Props = $props();
 </script>
 
 <div
     class="container mx-auto flex flex-col items-center gap-8 px-20 md:flex-row md:justify-between"
 >
-    {#each faqEntries as faqEntry}
-        <div class="max-w-md text-center md:w-1/2">
-            <h2 class="prose mb-2 text-xl font-bold dark:prose-invert md:text-lg">{faqEntry.title}</h2>
-            <p class="prose text-justify text-sm leading-relaxed dark:prose-invert md:text-base">
-                {@render faqEntry.paragraph()}
-            </p>
-        </div>
-    {/each}
+    {@render children?.()}
 </div>
---
+++ src/lib/components/FaqEntry.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+
+    interface Props {
+        title: string; // This could also be a Snippet
+        children?: Snippet;
+    }
+    const { title, children }: Props = $props();
+</script>
+
+<div class="max-w-md text-center md:w-1/2">
+    <h2 class="prose mb-2 text-2xl font-bold dark:prose-invert md:text-lg">{title}</h2>
+    <div class="prose text-justify text-sm leading-relaxed dark:prose-invert md:text-base">
+        {@render children?.()}
+    </div>
+</div>
