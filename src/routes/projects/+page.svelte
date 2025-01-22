67f7a29aab045919acd195347e4bbae244969dd7
Victor Edwin Reyes <victorereyesgg@gmail.com>
2025-01-22 18:38:43 +0800

    feat: integrate new Faq component into projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,16 +1,24 @@
 <script>
-    import Faq from '$lib/components/FAQ.svelte';
+    import Faq from '$lib/components/Faq.svelte';
     import FilterPagePanel from '$lib/components/FilterPagePanel.svelte';
     import projects from '$lib/projects/projects';
     const filters = ['All', 'Service', 'Innovation', 'Other'];
+    const faqEntries = [
+        {title: "GENERAL FAQ", paragraph: faqParagraph1},
+        {title: "GENERAL FAQ", paragraph: faqParagraph2}
+    ]
 </script>
 
 <article class="prose max-w-full dark:prose-invert">
     <FilterPagePanel {filters} cardsInfo={projects} />
 </article>
-<Faq
-    faq_title="GENERAL FAQ"
-    paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    faq_title1="GENERAL FAQ"
-    paragraph1="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-></Faq>
+
+{#snippet faqParagraph1()}
+    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+{/snippet}
+
+{#snippet faqParagraph2()}
+    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span>
+{/snippet}
+
+<Faq {faqEntries}/>
