64c6cc9aca64b256a53624af4e8ae95fe56c3325
Eriene Galinato <erienegalinato@gmail.com>
2025-03-20 17:06:47 +0800

    feat(Team): adapt exporter to exported JSON format

--- src/lib/data/team/index.ts
+++ src/lib/data/team/index.ts
@@ -1,18 +1,14 @@
 import { type Member, Member as MemberSchema } from '$lib/models/member';
-import { basename } from 'node:path';
+import type { EnhancedImgAttributes } from '@sveltejs/enhanced-img';
 import { parse } from 'valibot';
 
 export async function getTeam() {
-    const imports = import.meta.glob<Member>('./members/*.json');
+    const imports = import.meta.glob<Member>('./json/*.json');
 
-    const promises = Object.entries(imports).map(async ([path, asset]) => {
-        // Derive the image name from the original path
-        const base = basename(path, '.json');
-        const src: string = (await import(`$lib/people/team/images/${base}.webp?url`)).default;
-
+    const promises = Object.entries(imports).map(async ([_, asset]) => {
         const member = parse(MemberSchema, await asset());
+        const src: EnhancedImgAttributes['src'] = (await import(`$lib/assets/team/${member.img}.webp?enhanced?url`)).default;
 
-        // Type-safety enforced at build-time and run-time!
         return { ...member, src };
     });
 
