e58bffc9668c67d4d7afeb25c1eece24082ac209
Eriene Galinato <erienegalinato@gmail.com>
2025-03-20 09:43:29 +0800

    refactor(Team): prefer dedicated data folder

--- src/routes/people/+page.server.ts
+++ src/routes/people/+page.server.ts
@@ -1,5 +1,5 @@
 import { getExec } from '$lib/people/exec/exec';
-import { getTeam } from '$lib/people/team/team';
+import { getTeam } from '$lib/data/team';
 
 export async function load() {
     return { team: await getTeam(), exec: (await getExec()).reverse() };
