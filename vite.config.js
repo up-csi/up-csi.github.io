e8b2d256ad0e5a256beb8976b72a625e468795d0
Eriene Galinato <erienegalinato@gmail.com>
2025-04-08 20:51:09 +0800

    fix: remove `vite-plugin-tailwind-purgecss`

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [enhancedImages(), sveltekit(), tailwindcss(), purgeCss()],
+    plugins: [enhancedImages(), sveltekit(), tailwindcss()],
 });
