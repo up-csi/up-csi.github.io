a621efea23a72fc5bc76e6b18bdd2fae3e08db4a
Eriene Galinato <erienegalinato@gmail.com>
2025-04-16 18:50:13 +0800

    refactor(ContentContainer): fix tailwind colors and transitions

--- src/lib/components/panels/ContentContainer.svelte
+++ src/lib/components/panels/ContentContainer.svelte
@@ -10,12 +10,12 @@
 </script>
 
 <div
-    class="bg-csi-neutral-50 dark:bg-csi-neutral-950 flex flex-col items-center justify-between gap-12 rounded-3xl px-8 py-8 transition-colors ease-in-out md:px-24 md:py-16 xl:flex-row"
+    class="bg-muted text-muted-foreground flex flex-col items-center justify-between gap-12 rounded-3xl px-8 py-8 transition-colors ease-in-out md:px-24 md:py-16 xl:flex-row"
 >
-    <div class="flex flex-1 flex-col gap-4">
+    <div class="flex flex-1 flex-col gap-4 *:transition-colors *:ease-in-out">
         {@render text()}
     </div>
-    <div class="flex w-5/6 flex-col justify-center gap-4 md:flex-row xl:w-1/2">
+    <div class="flex w-5/6 flex-col justify-center gap-4 md:flex-row xl:w-1/2 *:transition-colors *:ease-in-out">
         {@render cards()}
     </div>
 </div>
