28111634fd306c79b6dc84b54da2d3148decfca0
Eriene Galinato <erienegalinato@gmail.com>
2025-04-16 18:51:41 +0800

    refactor(Accordion): fix tailwind colors and transitions

--- src/lib/components/accordions/Accordion.svelte
+++ src/lib/components/accordions/Accordion.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import ChevronDown from '@iconify/icons-heroicons/chevron-down';
+    import ChevronUp from '@iconify/icons-heroicons/chevron-up';
     import Icon from '@iconify/svelte';
     import type { Snippet } from 'svelte';
     import { slide } from 'svelte/transition';
@@ -18,26 +19,25 @@
     const { children, title }: Props = $props();
 </script>
 
-<div class="bg-csi-neutral-50 dark:bg-csi-neutral-900 w-full rounded-2xl shadow-lg">
+<div class="bg-float w-full rounded-2xl shadow-lg">
     <button
-        class="bg-csi-neutral-50 aria-expanded:bg-csi-neutral-100 dark:bg-csi-neutral-900 dark:aria-expanded:bg-csi-neutral-700 flex w-full items-center rounded-2xl px-5 py-4"
+        class="aria-expanded:bg-invert/10 flex w-full items-center rounded-2xl px-5 py-4"
         onclick={toggle}
         aria-expanded={open}
     >
-        <span class="font-inter text-csi-black dark:text-csi-white grow text-left text-lg font-bold"
+        <span class="font-inter text-float-foreground grow text-left text-lg font-bold"
             >{title}</span
         >
         <span class="grow-0">
             <Icon
-                icon={ChevronDown}
-                class="text-csi-black dark:text-csi-white size-5 transition-transform aria-expanded:rotate-90"
-                aria-expanded={open}
+                icon={(open) ? ChevronUp : ChevronDown}
+                class="text-float-foreground size-5"
             />
         </span>
     </button>
     {#if open}
         <div
-            class="text-csi-black dark:text-csi-white flex w-full items-center px-5 py-4"
+            class="text-float-foreground flex w-full items-center px-5 py-4"
             transition:slide
         >
             {@render children?.()}
