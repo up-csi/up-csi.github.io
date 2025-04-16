a0b749d7d4486c97385777165215f181a2d97e50
Eriene Galinato <erienegalinato@gmail.com>
2025-04-16 18:51:11 +0800

    refactor(GeneralInfo): fix tailwind colors and transitions

--- src/lib/components/miscellaneous/GeneralInfo.svelte
+++ src/lib/components/miscellaneous/GeneralInfo.svelte
@@ -1,5 +1,5 @@
 <div
-    class="bg-csi-neutral-50 dark:prose-invert dark:bg-csi-neutral-900 m-0 rounded-2xl px-8 py-8 shadow-lg lg:max-w-fit"
+    class="bg-float m-0 rounded-2xl px-8 py-8 shadow-lg lg:max-w-fit"
 >
     <div class="flex flex-col gap-5">
         <div>
@@ -9,7 +9,7 @@
         <div class="border-csi-blue bg-csi-blue w-full justify-center p-0.5"></div>
 
         <div>
-            <a class="underline" href="mailto:lino@up-csi.org">lino@up-csi.org</a>
+            <a class="text-foreground underline" href="mailto:lino@up-csi.org">lino@up-csi.org</a>
             <p class="m-0 max-w-full lg:max-w-80">
                 Department of Computer Science, UP Diliman, Diliman, Quezon City, Metro Manila
             </p>
