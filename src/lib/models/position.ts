27ff0934f316be8bea931f67379ab770e869d702
Eriene Galinato <erienegalinato@gmail.com>
2025-04-18 00:02:05 +0800

    feat: add `Position` schema

--- src/lib/models/position.ts
+++ src/lib/models/position.ts
@@ -1,3 +1,5 @@
+import { type InferOutput, picklist } from "valibot";
+
 export const POSITIONS = [
     'President',
     'Vice President for Innovation',
@@ -6,4 +8,10 @@ export const POSITIONS = [
     'Director for Membership & Internals',
     'Director for Branding & Creatives',
     'Director for Engineering',
+    '',
 ] as const;
+
+
+export const Position = picklist(POSITIONS);
+
+export type Position = InferOutput<typeof Position>;
