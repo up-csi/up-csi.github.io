b273dd2b4099d33d95df981613704fb37f7fec81
Eriene Galinato <erienegalinato@gmail.com>
2025-04-23 17:44:26 +0800

    feat: prefer dedicated trigger-modal components

---
+++ src/lib/components/items/EventItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import type { Event } from '$lib/models/event';
+    import Card from '$lib/components/cards/EventCard.svelte';
+
+    interface Props {
+        event: Event;
+    }
+    
+    const { event }: Props = $props();
+
+    let modalOpen = $state(false);
+</script>
+
+<button onclick={() => { modalOpen = true; }}>
+    <Card {event} />
+</button>
+
+<div class="absolute top-0 left-0">
+    <div class="fixed z-100 flex items-center justify-center h-screen w-screen bg-csi-black/50 {modalOpen ? 'block' : 'hidden'}">
+        <!-- TODO: insert modal here -->
+    </div>
+</div>
---
+++ src/lib/components/items/ProjectItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import type { Project } from '$lib/models/project';
+    import Card from '$lib/components/cards/ProjectCard.svelte';
+
+    interface Props {
+        project: Project;
+    }
+    
+    const { project }: Props = $props();
+
+    let modalOpen = $state(false);
+</script>
+
+<button onclick={() => { modalOpen = true; }}>
+    <Card {project} />
+</button>
+
+<div class="absolute top-0 left-0">
+    <div class="fixed z-100 flex items-center justify-center h-screen w-screen bg-csi-black/50 {modalOpen ? 'block' : 'hidden'}">
+        <!-- TODO: insert modal here -->
+    </div>
+</div>
