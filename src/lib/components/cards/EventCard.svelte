f4d62b6a33f251261328a56c91ade9f883f2f13b
Eriene Galinato <erienegalinato@gmail.com>
2025-04-23 17:31:26 +0800

    refactor(EventCard): update imports

--- src/lib/components/cards/EventCard.svelte
+++ src/lib/components/cards/EventCard.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
-    import type { Event } from '$lib/data/events';
-    const { src, event, state, type, dates, description, href }: Event = $props();
+    import type { Event } from '$lib/models/event';
+
+    interface Props {
+        event: Event;
+    }
+    
+    const { event }: Props = $props();
+    const { type, imgs, name, start_date, end_date, description }: Event = $derived(event);
+    const src = $derived.by(() => {return imgs ? imgs[0] : null});
 </script>
 
 <div
     class="bg-float text-float-foreground flex h-auto w-full flex-col overflow-hidden rounded-lg shadow-lg md:h-[32rem] md:w-72"
 >
-    <img {src} alt={event} loading="lazy" class="!m-0 h-64 shrink-0 object-cover md:h-48" />
+    {#if src}
+        <div class="**:!m-0">
+            <enhanced:img {src} alt={name} loading="lazy" class="h-64 shrink-0 object-cover md:h-48" />
+        </div>
+    {/if}
     <div class="!m-3 flex h-full flex-col justify-between gap-2 overflow-hidden *:!m-0">
-        <p>{state} - {type}</p>
-        <h2>{event}</h2>
+        <p>{type}</p>
+        <h2>{name}</h2>
         <!-- TODO: Use <time> element here for best accessibility. -->
-        <p>{dates}</p>
+        <p>{start_date} - {end_date}</p>
         <p class="grow overflow-hidden">{description}</p>
-        <p>
-            <a
-                {href}
-                class="text-float-foreground no-underline transition-colors ease-in-out hover:underline"
-                >More Info</a
-            >
-        </p>
     </div>
 </div>
