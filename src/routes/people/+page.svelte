25e270db3c9aba5d853bb27e1d5ac6e1ac6eb8cb
Eriene Galinato <erienegalinato@gmail.com>
2025-05-12 15:16:33 +0800

    feat(Exec): restyle display

--- src/routes/people/+page.svelte
+++ src/routes/people/+page.svelte
@@ -1,42 +1,34 @@
 <script>
     import { pres_term } from '$lib/data/exec';
 
+    import AccordionPanel from '$lib/components/panels/AccordionPanel.svelte';
+    import Exec from '$lib/components/accordions/Accordion.svelte';
     import ExecPanel from '$lib/components/panels/ExecPanel.svelte';
     import TeamPanel from '$lib/components/panels/TeamPanel.svelte';
 
     const { data } = $props();
     const { team, filteredTeams, exec } = $derived(data);
-    const pres_board = $derived(exec[pres_term]);
-</script>
 
-{#if pres_board}
-    <div class="pb-3">
-        <ExecPanel board={pres_board} />
-    </div>
-{/if}
+    const terms = $derived(Object.keys(exec).sort().reverse());
+</script>
 
 <div class="py-6">
     <TeamPanel {team} {filteredTeams} />
 </div>
 
 <section class="w-full">
-    <h1 class="text-foreground mb-4 w-full text-center text-3xl transition-colors md:text-4xl">
-        The Past Executive Boards
-    </h1>
+    <h1 class="pb-4 border-b-3 border-csi-blue mb-8 text-3xl text-foreground text-center">Meet the previous executive boards</h1>
 
-    {#each Object.keys(exec) as term}
-        {#if term !== pres_term}
-            {@const board = exec[term]}
-            {#if board}
-                <details class="py-4">
-                    <summary class="text-foreground text-xl transition-colors md:text-2xl"
-                        >{term}</summary
-                    >
-                    <div class="my-4">
+    <AccordionPanel>
+        {#each terms as term}
+            {#if term !== pres_term}
+                {@const board = exec[term]}
+                {#if board}
+                    <Exec title={term}>
                         <ExecPanel {board} />
-                    </div>
-                </details>
+                    </Exec>
+                {/if}
             {/if}
-        {/if}
-    {/each}
+        {/each}
+    </AccordionPanel>    
 </section>
