ab38c0336d1c4048aa4975de8a59b61df3171bf6
Louie Torres <dev@lowkeytowers.com>
2025-05-16 08:05:50 +0800

    feat(ProjectCard): fix height

--- src/lib/components/cards/ProjectCard.svelte
+++ src/lib/components/cards/ProjectCard.svelte
@@ -16,14 +16,14 @@
 </script>
 
 <div
-    class="bg-float text-float-foreground flex h-full w-96 flex-col overflow-hidden rounded-4xl p-4 shadow-lg md:max-w-72 md:p-3"
+    class="bg-float text-float-foreground flex h-72 w-96 flex-col overflow-hidden rounded-4xl p-4 shadow-lg md:max-w-72 md:p-3"
 >
     <div
-        class="md:text-md !m-3 flex h-full flex-col items-start justify-between gap-2 overflow-hidden *:!m-0"
+        class="md:text-md !m-3 flex h-full flex-col items-start justify-start gap-2 overflow-hidden *:!m-0"
     >
         <span class="bg-csi-blue-500 text-csi-black w-25 rounded-xl py-1">{tag}</span>
         <h2 class="text-left">{name}</h2>
         <p class="text-left text-lg">{[client, formattedDate].filter(v => v).join(' // ')}</p>
-        <p class="line-clamp-3 grow overflow-hidden text-left">{description}</p>
+        <p class="line-clamp-3 overflow-hidden text-left">{description}</p>
     </div>
 </div>
