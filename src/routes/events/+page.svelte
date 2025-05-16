371b9338f40e626e7548704a6d37797f45c395a4
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-05-16 15:15:34 +0800

    feat: add image carousel in events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import Events from '$lib/components/panels/EventPanel.svelte';
+    import ImageCarousel from '$lib/components/carousels/ImageCarousel.svelte';
 
     const { data } = $props();
     const { events, filteredEvents } = $derived(data);
 </script>
 
+<ImageCarousel items={events} />
 <section class="prose">
     <Events {events} {filteredEvents} />
 </section>
